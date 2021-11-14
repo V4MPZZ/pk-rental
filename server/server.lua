97ddf25865050b4eb0d59a6c7bd7eb856aea8f3d
PeeganKreece <63989752+PeeganKreece@users.noreply.github.com>
2021-11-14 04:55:10 -0500

    Config Price Functionality

--- server/server.lua
+++ server/server.lua
@@ -5,15 +5,14 @@ RegisterNetEvent('rental:server:paybike', function(data)
     local Player = QBCore.Functions.GetPlayer(src)
     local cash = Player.PlayerData.money['cash']
     local bank = Player.PlayerData.money['bank']
-    if data.bike == 'bmx' then price = 25.0
-        if cash >= price then
-            TriggerClientEvent("rental:client:spawnbike", src, data)
-            Player.Functions.RemoveMoney('cash', price)
-        elseif bank >= price then
-            TriggerClientEvent("rental:client:spawnbike", src, data)
-            Player.Functions.RemoveMoney('bank', price)
-        else
-            TriggerClientEvent('QBCore:Notify', src, "Not enough money to rent.", "error", "1500")
-        end
+    local price = data.price
+    if cash >= price then
+        TriggerClientEvent("rental:client:spawnbike", src, data)
+        Player.Functions.RemoveMoney('cash', price)
+    elseif bank >= price then
+        TriggerClientEvent("rental:client:spawnbike", src, data)
+        Player.Functions.RemoveMoney('bank', price)
+    else
+        TriggerClientEvent('QBCore:Notify', src, "Not enough money to rent.", "error", "1500")
     end
 end)
