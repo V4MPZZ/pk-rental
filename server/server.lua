d1ec83a81b7e77ad0cd5d6d7bdee0a5f81e4ecd4
PeeganKreece <63989752+PeeganKreece@users.noreply.github.com>
2021-11-14 03:59:25 -0500

    Rental Functionality Finish

--- server/server.lua
+++ server/server.lua
@@ -2,18 +2,18 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('rental:server:paybike', function(data)
     local src = source
-    local xPlayer = QBCore.Functions.GetPlayer(src)
-    local coords = vector3(-249.0394, -952.4588, 31.220005)
+    local Player = QBCore.Functions.GetPlayer(src)
+    local cash = Player.PlayerData.money['cash']
+    local bank = Player.PlayerData.money['bank']
     if data.bike == 'bmx' then price = 25.0
-        if xPlayer.Functions.RemoveMoney('cash', price) then
-            QBCore.Functions.SpawnVehicle('bmx', function(veh)
-                SetVehicleNumberPlateText(veh, "RENT"..tostring(math.random(1000, 9999)))
-                exports['LegacyFuel']:SetFuel(veh, 100.0)
-                SetEntityHeading(veh, 250.0)
-                TriggerClientEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
-            end, coords, true)
+        if cash >= price then
+            TriggerClientEvent("rental:client:spawnbike", src, data)
+            Player.Functions.RemoveMoney('cash', price)
+        elseif bank >= price then
+            TriggerClientEvent("rental:client:spawnbike", src, data)
+            Player.Functions.RemoveMoney('bank', price)
         else
-            QBCore.Functions.Notify("Not enough money to rent.", "error", "1500")
+            TriggerClientEvent('QBCore:Notify', src, "Not enough money to rent.", "error", "1500")
         end
     end
 end)
